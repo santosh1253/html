0ca120ea53c1b3e835f99676bebf6fc88d5dd227
Guru <konkimalla1253@gmail.com>
2024-08-05 08:23:05 +0530

    HTML Files

---
+++ file.js
@@ -0,0 +1,15 @@
+var emp=[
+    // {
+    //     id:1,name:"Guru",age:25,isActive:true,salary:40000
+    // },
+    // {
+    //     id:2,name:"Pavan",age:25,isActive:false,salary:45000
+    // },
+    // {
+    //     id:3,name:"Nari",age:25,isActive:true,salary:44000
+    // },
+    "Guru","Datta"
+    ,40,50,90
+]
+emp.unshift("Konkimalla");
+console.log(`emp is ${emp}`);
